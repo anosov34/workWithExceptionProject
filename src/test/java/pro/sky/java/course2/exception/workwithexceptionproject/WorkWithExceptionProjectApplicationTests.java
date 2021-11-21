de494334a7d77701e19a30bf25a081706a60f7a1
anosov34 <bookmake339@gmail.com>
2021-11-21 14:55:14 +0300

    Initial commit

---
+++ src/test/java/pro/sky/java/course2/exception/workwithexceptionproject/WorkWithExceptionProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.exception.workwithexceptionproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkWithExceptionProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

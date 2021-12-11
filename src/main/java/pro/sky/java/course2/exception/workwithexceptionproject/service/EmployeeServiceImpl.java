2c6c31cfd44c279cebc77a0d048d096e9ef59e0b
anosov34 <bookmake339@gmail.com>
2021-12-11 21:24:02 +0300

    2.4 fix

--- src/main/java/pro/sky/java/course2/exception/workwithexceptionproject/service/EmployeeServiceImpl.java
+++ src/main/java/pro/sky/java/course2/exception/workwithexceptionproject/service/EmployeeServiceImpl.java
@@ -5,15 +5,50 @@ import pro.sky.java.course2.exception.workwithexceptionproject.Employee;
 import pro.sky.java.course2.exception.workwithexceptionproject.exception.ArraysIsFullException;
 import pro.sky.java.course2.exception.workwithexceptionproject.exception.EmployeeIsNotFoundException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
     private List<Employee> employees;
 
-    public EmployeeServiceImpl(List<Employee> employees) {
-        this.employees = employees;
+    public EmployeeServiceImpl() {
+        this.employees = new ArrayList<>();
+        employees.add(new Employee("Sergey", "Sergeev"));
+        employees.add(new Employee("Ivan", "Ivanov"));
+        employees.add(new Employee("Andrey", "Andreyev"));
     }
+    @Override
+    public boolean addEmployeeVideo(String firstName, String lastName) {
+        return employees.add(new Employee(firstName, lastName));
+    }
+
+    @Override
+    public boolean removeEmployeeVideo(String firstName, String lastName) {
+        return employees.remove(new Employee(firstName, lastName));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     public Employee addEmployee(String firstName, String lastName) {
         Employee employee = new Employee(firstName, lastName);
@@ -43,6 +78,8 @@ public class EmployeeServiceImpl implements EmployeeService {
         }
         throw new EmployeeIsNotFoundException();
     }
+
+
 
 //        Employee employeeForFind = new Employee(firstName, lastName);
 //        for (int i = 0; i < employees.length; i++) {

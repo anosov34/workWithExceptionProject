6614de4ada28fade9c0340efbac2fcd22e044e78
anosov34 <bookmake339@gmail.com>
2021-12-12 19:07:08 +0300

    2.4 fix

--- src/main/java/pro/sky/java/course2/exception/workwithexceptionproject/service/EmployeeService.java
+++ src/main/java/pro/sky/java/course2/exception/workwithexceptionproject/service/EmployeeService.java
@@ -6,10 +6,10 @@ import java.util.List;
 
 public interface EmployeeService {
     Employee addEmployee(String firstName, String lastName);
-    Employee removeEmployee(String firstName, String lastName);
-    Employee findEmployee(String firstName, String lastName);
-    List<Employee> getEmployees();
 
-//    boolean addEmployeeVideo(String firstName, String lastName);
-//    boolean removeEmployeeVideo(String firstName, String lastName);
+    Employee removeEmployee(String firstName, String lastName);
+
+    Employee findEmployee(String firstName, String lastName);
+
+    List<Employee> getEmployees();
 }
